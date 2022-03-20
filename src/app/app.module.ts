12e2cc086ae8e8fe4debf01a04a0dfb00858bc6a
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2022-03-20 14:55:28 +0000

    Add angular cdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LayoutModule } from '@angular/cdk/layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,17 +9,9 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { SharedModule } from './shared';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavigationComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SharedModule
-  ],
+  declarations: [AppComponent, NavigationComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
