308f39ba3e44e6eedadf4861ea602456d814bffd
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2022-03-20 15:44:50 +0000

    Make search component responsive

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { BreakpointService } from 'src/app/core/services/breakpoint.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SearchComponent implements OnInit {
   form!: FormGroup;
-  constructor(private readonly formBuilder: FormBuilder) {}
+  isHandset$: Observable<boolean>;
+
+  constructor(private readonly formBuilder: FormBuilder, private readonly breakpointService: BreakpointService) {
+    this.isHandset$ = this.breakpointService.isHandset$;
+  }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       what: [''],
-      where: [''],
+      where: ['']
     });
   }
 }
